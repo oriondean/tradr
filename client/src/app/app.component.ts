7a310f76e9a09b4ea0d90c0b496e74bb3733d014
Dean Kerr <dkerr@scottlogic.com>
2024-04-23 14:15:03 +0100

    Add layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { OrderFormComponent } from '../order-form/order-form.component';
 import { PublicOrdersComponent } from './components/public-orders/public-orders.component';
 import { Client } from '@stomp/stompjs';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { PrivateOrderBookComponent } from './components/private-order-book/private-order-book.component';
+import { TradeHistoryComponent } from './components/trade-history/trade-history.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OrderFormComponent, PublicOrdersComponent, MatToolbarModule, PrivateOrderBookComponent],
+  imports: [RouterOutlet, OrderFormComponent, PublicOrdersComponent, MatToolbarModule, PrivateOrderBookComponent, TradeHistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
