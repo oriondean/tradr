d3edad207c1843c7315f2c48395fd7d41f0071e0
jkeddy-scottlogic <jkeddy@scottlogic.com>
2024-04-23 14:21:30 +0100

    Material table, not finished

--- client/src/app/components/public-orders/public-orders.component.ts
+++ client/src/app/components/public-orders/public-orders.component.ts
@@ -2,18 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PublicOrdersService } from '../../services/public-orders/public-orders.service';
 import { Client } from '@stomp/stompjs';
-
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-public-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './public-orders.component.html',
   styleUrl: './public-orders.component.css',
 })
 export class PublicOrdersComponent {
   askOrders: Map<number, number> = new Map<number, number>();
   bidOrders: Map<number, number> = new Map<number, number>();
+  displayedColumns = ['quantity', 'price'];
 
   constructor(private orderService: PublicOrdersService) {
     this.orderService.getAsks().subscribe((asks) => (this.askOrders = asks));
