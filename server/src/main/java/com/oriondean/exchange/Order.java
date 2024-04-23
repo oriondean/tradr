3378807f82ab5f00884cf5c0afa8a1c0aadf6712
Dean Kerr <dkerr@scottlogic.com>
2024-04-23 16:42:30 +0100

    Fix issue matching partial orders

--- server/src/main/java/com/oriondean/exchange/Order.java
+++ server/src/main/java/com/oriondean/exchange/Order.java
@@ -63,8 +63,8 @@ public class Order {
         return isBid() ? price > order.price : price < order.price;
     }
 
-    public Order reduceQuantity(Integer quantity) {
-        return new Order(id, price, quantity - quantity, action, account, initialQuantity);
+    public Order reduceQuantity(Integer toReduce) {
+        return new Order(id, price, quantity - toReduce, action, account, initialQuantity);
     }
 
     public boolean equals(Object o) {
