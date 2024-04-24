8e21b57960a9eda88236709f805522c9faf20e56
jkeddy-scottlogic <jkeddy@scottlogic.com>
2024-04-24 13:20:03 +0100

    Added last updated

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { TimeagoModule } from 'ngx-timeago';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()],
+  providers: [
+    importProvidersFrom(TimeagoModule.forRoot()),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+  ],
 };
--- client/src/app/components/public-orders/public-orders.component.ts
+++ client/src/app/components/public-orders/public-orders.component.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PublicOrdersService } from '../../services/public-orders/public-orders.service';
-import { Client } from '@stomp/stompjs';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { StompService } from '../../services/api/Stomp.service';
+import { TimeagoModule } from 'ngx-timeago';
 
 @Component({
   selector: 'app-public-orders',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatTableModule],
+  imports: [CommonModule, MatCardModule, MatTableModule, TimeagoModule],
   templateUrl: './public-orders.component.html',
   styleUrl: './public-orders.component.css',
 })
@@ -19,6 +19,7 @@ export class PublicOrdersComponent {
   displayedColumns = ['price', 'quantity'];
   bidDataSource = Object.entries(this.bidOrders);
   askDataSource = Object.entries(this.askOrders);
+  lastUpdated?: Date;
 
   constructor(
     private orderService: PublicOrdersService,
@@ -27,20 +28,24 @@ export class PublicOrdersComponent {
     this.orderService.getAsks().subscribe((asks) => {
       this.askOrders = asks;
       this.askDataSource = Object.entries(this.askOrders);
+      this.lastUpdated = new Date();
     });
     this.orderService.getBids().subscribe((bids) => {
       this.bidOrders = bids;
       this.bidDataSource = Object.entries(this.bidOrders);
+      this.lastUpdated = new Date();
     });
 
-    stompService.subscribe('/topic/public/bids', (bids) => {
+    this.stompService.subscribe('/topic/public/bids', (bids) => {
       this.bidOrders = JSON.parse(bids.body);
       this.bidDataSource = Object.entries(this.bidOrders);
+      this.lastUpdated = new Date();
     });
 
-    stompService.subscribe('/topic/public/asks', (asks) => {
+    this.stompService.subscribe('/topic/public/asks', (asks) => {
       this.askOrders = JSON.parse(asks.body);
       this.askDataSource = Object.entries(this.askOrders);
+      this.lastUpdated = new Date();
     });
   }
 }
