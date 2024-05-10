eb91e4a11961f96f457c3be4920bf07dbdbffe54
gramzanali-scottlogic <125259276+gramzanali-scottlogic@users.noreply.github.com>
2024-05-10 11:07:18 +0100

    UI tables in, not functioning

--- server/src/main/java/com/oriondean/exchange/TradeRepository.java
+++ server/src/main/java/com/oriondean/exchange/TradeRepository.java
@@ -2,5 +2,9 @@ package com.oriondean.exchange;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 interface TradeRepository extends JpaRepository<Trade, Integer> {
+
+    List<Trade> findAllByAggressor(String aggressor);
 }
